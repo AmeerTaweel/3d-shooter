65e448a94755446f7ffd80c677f4aefce0a8ce69
Ameer Taweel <ameertaweel2002@gmail.com>
2022-04-21 02:12:59 +0300

    feat: finish level 1

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -20,9 +20,14 @@ public class PlayerController : MonoBehaviour
     [Tooltip("The player shooter script that fires projectiles")]
     public Shooter playerShooter;
 
+    public Health health;
+    public List<GameObject> disableWhileDead;
+
     // The character controller component on the player
     private CharacterController controller;
     private InputManager inputmanager;
+
+    private bool doubleJumpAvailable = false;
 
     /// <summary>
     /// Description:
@@ -62,6 +67,72 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
+        if (health.currentHealth < 0)
+        {
+            foreach (var obj in disableWhileDead)
+            {
+                obj.SetActive(false);
+            }
 
+            return;
+        }
+        foreach (var obj in disableWhileDead)
+        {
+            obj.SetActive(true);
+        }
+        ProcessMovement();
+        ProcessRotation();
+    }
+
+    private Vector3 moveDirection;
+    void ProcessMovement()
+    {
+        float horizontalMovement = inputmanager.horizontalMoveAxis;
+        float verticalMovement = inputmanager.verticalMoveAxis;
+        bool isJumpPressed = inputmanager.jumpPressed;
+
+        if (controller.isGrounded)
+        {
+            doubleJumpAvailable = true;
+            moveDirection = new Vector3(horizontalMovement, 0, verticalMovement);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed;
+
+            if (isJumpPressed)
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(horizontalMovement * moveSpeed, moveDirection.y, verticalMovement * moveSpeed);
+            moveDirection = transform.TransformDirection(moveDirection);
+            
+            if (isJumpPressed && doubleJumpAvailable)
+            {
+                moveDirection.y = jumpPower;
+                doubleJumpAvailable = false;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            moveDirection.y = -0.3f;
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void ProcessRotation()
+    {
+        float horizontalRotationInput = inputmanager.horizontalLookAxis;
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(
+            playerRotation.x,
+            playerRotation.y + horizontalRotationInput * lookSpeed * Time.deltaTime,
+            playerRotation.z
+        ));
     }
 }
